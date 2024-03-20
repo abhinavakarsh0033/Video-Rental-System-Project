ea102829b5cb6348b21b474b8a24974d6b2a2420
codaks14 <akshatstar1410@gmail.com>
2024-03-20 20:09:56 +0530

    NEW DAY

--- vrs/home/urls.py
+++ vrs/home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("home",views.home,name='home'),
     path("contact",views.contact,name='contact'),
     path("signup",views.signup,name='signup'),
+    path("signout",views.signout,name='signout')
 ]
--- vrs/home/views.py
+++ vrs/home/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return redirect('/home')
     if request.method=='POST':
         user = authenticate(username=request.POST.get('email'),password=request.POST.get('password'))
         print(request.POST.get('email'), request.POST.get('password'))
@@ -16,6 +20,21 @@ def index(request):
             return render(request,'index.html')
             
     return render(request,'index.html')
+
+def signup(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            # password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username,password=password)
+            messages.success(request, 'Account was created successfully')
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request,'signup.html',{'form':form})
 
 def about(request):
     return render(request,'about.html')
@@ -29,5 +48,7 @@ def home(request):
 def contact(request):
     return render(request,'contact.html')
 
-def signup(request):
-    return render(request,'signup.html')
+def signout(request):
+    print(1)
+    logout(request)
+    return redirect('/')
